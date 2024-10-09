8f4b3f3afcb89a04baa89b19e923339d8ea13777
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2024-10-09 09:54:11 +0200

    New nvim configuration based on lua

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,37 @@
+return {
+	"nvim-telescope/telescope.nvim",
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' },
+    "nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+
+		telescope.setup({
+			defaults = {
+				path_display = { "smart" },
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous, -- move to prev result
+						["<C-j>"] = actions.move_selection_next, -- move to next result
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+					},
+				},
+			},
+		})
+
+		-- telescope.load_extension("fzf")
+
+		local builtin = require('telescope.builtin')
+		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep (work dir)' })
+		vim.keymap.set('n', '<leader>fs', builtin.grep_string, { desc = 'Telescope grep string' })
+		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+		vim.keymap.set('n', '<leader>fu', builtin.current_buffer_fuzzy_find, { desc = 'Telescope current buffer fuzzy find' })
+		vim.keymap.set('n', '<leader>ft', builtin.current_buffer_tags, { desc = 'Telescope current buffer tags' })
+	end
+}
+
---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,110 @@
+local M = {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+      require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "haskell" },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },  
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<Leader>ss",
+            node_incremental = "<Leader>si",
+            scope_incremental = "<Leader>sc",
+            node_decremental = "<bs>",
+          },
+        },
+        textobjects = {
+          select = {
+            enable = true,
+
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              -- You can optionally set descriptions to the mappings (used in the desc parameter of
+              -- nvim_buf_set_keymap) which plugins like which-key display
+              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+              -- You can also use captures from other query groups like `locals.scm`
+              ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+            },
+            -- You can choose the select mode (default is charwise 'v')
+            --
+            -- Can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * method: eg 'v' or 'o'
+            -- and should return the mode ('v', 'V', or '<c-v>') or a table
+            -- mapping query_strings to modes.
+            selection_modes = {
+              ['@parameter.outer'] = 'v', -- charwise
+              ['@function.outer'] = 'V', -- linewise
+              ['@class.outer'] = '<c-v>', -- blockwise
+            },
+            -- If you set this to `true` (default is `false`) then any textobject is
+            -- extended to include preceding or succeeding whitespace. Succeeding
+            -- whitespace has priority in order to act similarly to eg the built-in
+            -- `ap`.
+            --
+            -- Can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * selection_mode: eg 'v'
+            -- and should return true or false
+            include_surrounding_whitespace = true,
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              ["]f"] = { query = "@call.outer", desc = "Next function call start" },
+              ["]m"] = { query = "@function.outer", desc = "Next method/function def start" },
+              ["]c"] = { query = "@class.outer", desc = "Next class start" },
+              ["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
+              ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
+
+              -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+              -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+              -- ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+              -- ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+            },
+            goto_next_end = {
+              ["]F"] = { query = "@call.outer", desc = "Next function call end" },
+              ["]M"] = { query = "@function.outer", desc = "Next method/function def end" },
+              ["]C"] = { query = "@class.outer", desc = "Next class end" },
+              ["]I"] = { query = "@conditional.outer", desc = "Next conditional end" },
+              ["]L"] = { query = "@loop.outer", desc = "Next loop end" },
+            },
+            goto_previous_start = {
+              ["[f"] = { query = "@call.outer", desc = "Prev function call start" },
+              ["[m"] = { query = "@function.outer", desc = "Prev method/function def start" },
+              ["[c"] = { query = "@class.outer", desc = "Prev class start" },
+              ["[i"] = { query = "@conditional.outer", desc = "Prev conditional start" },
+              ["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
+            },
+            goto_previous_end = {
+              ["[F"] = { query = "@call.outer", desc = "Prev function call end" },
+              ["[M"] = { query = "@function.outer", desc = "Prev method/function def end" },
+              ["[C"] = { query = "@class.outer", desc = "Prev class end" },
+              ["[I"] = { query = "@conditional.outer", desc = "Prev conditional end" },
+              ["[L"] = { query = "@loop.outer", desc = "Prev loop end" },
+            },
+          },
+        },
+      })
+    end
+  }, {
+    "nvim-treesitter/nvim-treesitter-textobjects"
+  }
+}
+
+return { M }
